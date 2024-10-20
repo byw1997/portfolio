33bb0fe048404cec3b3ee27ffbcc27f03a6f7a52
byw1997 <72117733+byw1997@users.noreply.github.com>
2024-10-20 23:34:58 +0900

    Delete P2_ArmyChess directory

--- P2_ArmyChess/Scripts/Board/Highlight.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Highlight : MonoBehaviour
-{
-    internal bool highlighted = false;
-    internal List<int[]> highlightedSquares = new List<int[]>();
-}
